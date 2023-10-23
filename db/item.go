509c59b15713e1780967db4d8e521bd42f2ce4ed
Ryan Waszak <rwaszak@tesla.com>
2023-10-23 08:30:23 -0700

    Move item struct to models package and add item db package for crud functions

--- db/item.go
+++ db/item.go
@@ -1,32 +1,92 @@
-package models
+package db
 
 import (
-    "fmt"
-    "net/http"
+    "database/sql"
+    "github.com/rwas2505/go-chi/models"
 )
 
-type Item struct {
-    ID int `json:"id"`
-    Name string `json:"name"`
-    Description string `json:"description"`
-    CreatedAt string `json:"created_at"`
-}
+func (db Database) GetAllItems() (*models.ItemList, error) {
+    list := &models.ItemList{}
 
-type ItemList struct {
-    Items []Item `json:"items"`
-}
+    rows, err := db.Conn.Query("SELECT * FROM items ORDER BY ID DESC")
 
-func (i *Item) Bind(r *http.Request) error {
-    if i.Name == "" {
-        return fmt.Errorf("name is a required field")
+    if err != nil {
+        return list, err
     }
+
+    for rows.Next() {
+        var item models.Item
+        err := rows.Scan(&item.ID, &item.Name, &item.Description, &item.CreatedAt)
+
+        if err != nil {
+            return list, err
+        }
+
+        list.Items = append(list.Items, item)
+    }
+
+    return list, nil
+}
+
+func (db Database) AddItem(item *models.Item) error {
+    var id int
+
+    var createdAt string
+
+    query := `INSERT INTO items (name, description) VALUES ($1, $2) RETURNING id, created_at`
+
+    err := db.Conn.QueryRow(query, item.Name, item.Description).Scan(&id, &createdAt)
+
+    if err != nil {
+        return err
+    }
+
+    item.ID = id
+    item.CreatedAt = createdAt
+
     return nil
 }
 
-func (*ItemList) Render(w http.ResponseWriter, r *http.Request) error {
-    return nil
+func (db Database) GetItemById(itemId int) (models.Item, error) {
+    item := models.Item{}
+
+    query := `SELECT * FROM items WHERE id = $1;`
+
+    row := db.Conn.QueryRow(query, itemId)
+
+    switch err := row.Scan(&item.ID, &item.Name, &item.Description, &item.CreatedAt); err {
+    case sql.ErrNoRows:
+        return item, ErrNoMatch
+    default:
+        return item, err
+    }
 }
 
-func (*Item) Render(w http.ResponseWriter, r *http.Request) error {
-    return nil
+func (db Database) DeleteItem(itemId int) error {
+    query := `DELETE FROM items WHERE id = $1;`
+
+    _, err := db.Conn.Exec(query, itemId)
+
+    switch err {
+    case sql.ErrNoRows:
+        return ErrNoMatch
+    default:
+        return err
+    }
+}
+
+func (db Database) UpdateItem(itemId int, itemData models.Item) (models.Item, error) {
+    item := models.Item{}
+    query := `UPDATE items SET name=$1, description=$2 WHERE id=$3 RETURNING id, name, description, created_at;`
+
+    err := db.Conn.QueryRow(query, itemData.Name, itemData.Description, itemId).Scan(&item.ID, &item.Name, &item.Description, &item.CreatedAt)
+
+    if err != nil {
+        if err == sql.ErrNoRows {
+            return item, ErrNoMatch
+        }
+        return item, err
+    }
+	
+    return item, nil
 }
