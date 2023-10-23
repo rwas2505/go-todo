563ad616f902e312d8ae900d29011434176b1d16
Ryan Waszak <rwaszak@tesla.com>
2023-10-23 08:49:28 -0700

    Adds hanlder/items.go with create item functionality working from postman

---
+++ handler/items.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+    "context"
+    "fmt"
+    "net/http"
+    "strconv"
+    "github.com/go-chi/chi"
+    "github.com/go-chi/render"
+    "github.com/rwas2505/go-chi/db"
+    "github.com/rwas2505/go-chi/models"
+)
+
+var itemIDKey = "itemID"
+
+func items(router chi.Router) {
+    router.Get("/", getAllItems)
+    router.Post("/", createItem)
+    router.Route("/{itemId}", func(router chi.Router) {
+        router.Use(ItemContext)
+        router.Get("/", getItem)
+        router.Put("/", updateItem)
+        router.Delete("/", deleteItem)
+    })
+}
+
+func ItemContext(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        itemId := chi.URLParam(r, "itemId")
+
+        if itemId == "" {
+            render.Render(w, r, ErrorRenderer(fmt.Errorf("item ID is required")))
+            return
+        }
+
+        id, err := strconv.Atoi(itemId)
+
+        if err != nil {
+            render.Render(w, r, ErrorRenderer(fmt.Errorf("invalid item ID")))
+        }
+
+        ctx := context.WithValue(r.Context(), itemIDKey, id)
+
+        next.ServeHTTP(w, r.WithContext(ctx))
+    })
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+    item := &models.Item{}
+
+    if err := render.Bind(r, item); err != nil {
+        render.Render(w, r, ErrBadRequest)
+        return
+    }
+
+    if err := dbInstance.AddItem(item); err != nil {
+        render.Render(w, r, ErrorRenderer(err))
+        return
+    }
+	
+    if err := render.Render(w, r, item); err != nil {
+        render.Render(w, r, ServerErrorRenderer(err))
+        return
+    }
+}
